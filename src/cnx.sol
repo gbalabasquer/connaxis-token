70df379d524adb318c33eebf290e82685bb920eb
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-08-02 09:51:43 -0300

    Initial commit

--- src/cnx.sol
+++ src/cnx.sol
@@ -1,74 +1,55 @@
 pragma solidity ^0.4.13;
 
 import "ds-token/token.sol";
-import "ds-roles/roles.sol";
 
 contract Cnx is DSToken("CNX") {
     uint8 public i;
-    mapping (uint256 => prize) public prizes;
+    mapping (uint256 => request) public requests;
 
-    event LogPurchase(address owner, uint256 prize);
-    event LogPrize(uint256 pos, string title, string photo, uint128 cost, uint8 quantity);
+    event LogPurchase(address indexed receiver, uint256 pos);
+    event LogRequest(address indexed receiver, uint256 pos, string title, string photo, uint128 cost);
 
     bool exchange = false;
 
-    struct prize {
+    struct request {
         string title;
         string photo;
         uint128 cost;
-        uint8 quantity;
+        address receiver;
+        bool exchanged;
     }
 
     function Cnx() {
         setName("10th Anniversary Connaxis Token");
-        mint(100000 * 10 ** 18);
-
-        DSRoles roles = new DSRoles();
-        setAuthority(roles);
-
-        roles.setRoleCapability(1, this, bytes4(sha3("setUserRole(address,uint8,bool)")), true);
-        roles.setRoleCapability(1, this, bytes4(sha3("startExchange()")), true);
-        roles.setRoleCapability(1, this, bytes4(sha3("stopExchange()")), true);
-        roles.setRoleCapability(1, this, bytes4(sha3("addPrize(string,string,uint128,uint8)")), true);
-        roles.setRoleCapability(1, this, bytes4(sha3("editPrize(uint256,string,string,uint128,uint8)")), true);
-        roles.setRoleCapability(1, this, bytes4(sha3("exchangePrize(uint256)")), true);
-        roles.setRoleCapability(2, this, bytes4(sha3("exchangePrize(uint256)")), true);
-
-        roles.setUserRole(msg.sender, 1, true);
-
-        roles.setAuthority(roles);
-        roles.setOwner(msg.sender);
     }
 
-    function addPrize(string title, string photo, uint128 cost, uint8 quantity) auth {
-        editPrize(++i, title, photo, cost, quantity);
+    function setPrizeVariables(uint256 pos, string title, string photo, uint128 cost, address receiver) internal {
+        requests[pos].title = title;
+        requests[pos].photo = photo;
+        requests[pos].cost = cost;
+        requests[pos].receiver = receiver;
+        requests[pos].exchanged = false;
+        LogRequest(receiver, pos, title, photo, cost);
     }
 
-    function editPrize(uint256 pos, string title, string photo, uint128 cost, uint8 quantity) auth {
-        prizes[pos].title = title;
-        prizes[pos].photo = photo;
-        prizes[pos].cost = cost;
-        prizes[pos].quantity = quantity;
-        LogPrize(pos, title, photo, cost, quantity);
+    function addRequestExchange(string title, string photo, uint128 cost, address receiver) auth {
+        setPrizeVariables(++i, title, photo, cost, receiver);
     }
 
-    function startExchange() auth {
-        exchange = true;
+    function editRequestExchange(uint256 pos, string title, string photo, uint128 cost, address receiver) auth {
+        assert(!requests[pos].exchanged);
+        setPrizeVariables(pos, title, photo, cost, receiver);
     }
 
-    function stopExchange() auth {
-        exchange = false;
-    }
+    function acceptExchange(uint256 pos) auth {
+        assert(!requests[pos].exchanged);
+        assert(msg.sender == requests[pos].receiver);
 
-    function exchangePrize(uint256 id) auth {
-        assert(exchange);
+        assert(_balances[msg.sender] >= requests[pos].cost);
 
-        assert(prizes[id].quantity >= 1);
-        assert(_balances[msg.sender] >= prizes[id].cost);
+        burn(requests[pos].cost);
+        requests[pos].exchanged = true;
 
-        burn(prizes[id].cost);
-        prizes[i].quantity--;
-
-        LogPurchase(msg.sender, id);
+        LogPurchase(msg.sender, pos);
     }
 }
--- src/cnx.t.sol
+++ src/cnx.t.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.4.8;
 import "ds-test/test.sol";
 
 import "./cnx.sol";
+import "ds-roles/roles.sol";
+import "ds-vault/vault.sol";
 
 contract Test is DSTest {
     Cnx cnx;
+    DSVault vault;
     FakePerson admin;
     FakePerson u1;
     FakePerson u2;
@@ -13,90 +16,134 @@ contract Test is DSTest {
 
     function setUp() {
         cnx = new Cnx();
-        admin = new FakePerson(cnx);
+        vault = new DSVault();
+        admin = new FakePerson(cnx, vault);
 
-        var roles = DSRoles(address(cnx.authority()));
+        DSRoles roles = new DSRoles();
+        cnx.setAuthority(roles);
+        vault.setAuthority(roles);
+
+        roles.setRoleCapability(1, vault, bytes4(sha3("mint(uint128)")), true);
+        roles.setRoleCapability(1, vault, bytes4(sha3("burn(uint128)")), true);
+        roles.setRoleCapability(1, vault, bytes4(sha3("pull(address,uint128)")), true);
+        roles.setRoleCapability(1, vault, bytes4(sha3("push(address,uint128)")), true);
+        roles.setRoleCapability(1, cnx, bytes4(sha3("setUserRole(address,uint8,bool)")), true);
+        roles.setRoleCapability(1, cnx, bytes4(sha3("addRequestExchange(string,string,uint128,address)")), true);
+        roles.setRoleCapability(1, cnx, bytes4(sha3("editRequestExchange(uint256,string,string,uint128,address)")), true);
+        roles.setRoleCapability(1, cnx, bytes4(sha3("acceptExchange(uint256)")), true);
+        roles.setRoleCapability(2, cnx, bytes4(sha3("acceptExchange(uint256)")), true);
+        roles.setRoleCapability(3, cnx, bytes4(sha3("mint(uint128)")), true);
+        roles.setRoleCapability(3, cnx, bytes4(sha3("burn(uint128)")), true);
+
+        roles.setUserRole(this, 1, true);
+        roles.setUserRole(vault, 3, true);
+
+        roles.setAuthority(roles);
+        roles.setOwner(this);
+
+        vault.swap(cnx);
+        vault.mint(100000 * 10 ** 18);
 
         roles.setUserRole(admin, 1, true); 
-        u1 = new FakePerson(cnx);
-        u2 = new FakePerson(cnx);
+        u1 = new FakePerson(cnx, vault);
+        u2 = new FakePerson(cnx, vault);
         roles.setUserRole(u1, 2, true); 
         roles.setUserRole(u2, 2, true); 
-        u3 = new FakePerson(cnx);
+        u3 = new FakePerson(cnx, vault);
 
-        cnx.addPrize('Iphone 7', 'http://blahblah', 1000 * 10 ** 18, 2);
-
+        cnx.addRequestExchange('Iphone 7', 'http://blahblah', 1000 * 10 ** 18, u1);
+        vault.push(this, 50000 * 10 ** 18);
         cnx.transfer(u1, 10000 * 10 ** 18);
-        cnx.transfer(u2, 10000 * 10 ** 18);
+        cnx.transfer(u2, 999 * 10 ** 18);
+    }
+
+    function testPushAdmin() {
+        admin.push(u2, 500 * 10 ** 18);
+    }
+
+    function testFailPushUser() {
+        u1.push(u2, 500 * 10 ** 18);
     }
 
     function testTransfer() {
+        log_named_uint('balance', cnx.balanceOf(this));
         u1.transfer(u2, 500 * 10 ** 18);
     }
 
-    function testAddPrize() {
-        admin.addPrize('Airpods', 'http://blahblah', 1000 * 10 ** 18, 2);
+    function testAddRequestExchangeAdmin() {
+        admin.addRequestExchange('Airpods', 'http://blahblah', 1000 * 10 ** 18, 0x0);
     }
 
-    function testEditPrize() {
-        admin.editPrize(1, 'Iphone 6', 'http://blahblah', 1000 * 10 ** 18, 2);
+    function testEditRequestExchangeAdmin() {
+        admin.editRequestExchange(1, 'Iphone 6', 'http://blahblah', 1000 * 10 ** 18, 0x0);
     }
 
-    function testFailAddPrize() {
-        u1.addPrize('BigBox', 'http://blahblah', 1000 * 10 ** 18, 2);
+    function testFailAddRequestExchangeUser() {
+        u1.addRequestExchange('BigBox', 'http://blahblah', 1000 * 10 ** 18, 0x0);
     }
 
-    function testFailEditPrize() {
-        u1.editPrize(1, 'Iphone 6', 'http://blahblah', 1000 * 10 ** 18, 2);
+    function testFailEditRequestExchangeUser() {
+        u1.editRequestExchange(1, 'Iphone 6', 'http://blahblah', 1000 * 10 ** 18, 0x0);
     }
 
-    function testExchangePrize() {
-        cnx.startExchange();
-        assertEq(cnx.balanceOf(this), 80000 * 10 ** 18);
-        var (,,,quantity) = cnx.prizes(1);
-        assertEq(uint(quantity), 2);
-        cnx.exchangePrize(1);
-        assertEq(cnx.balanceOf(this), 79000 * 10 ** 18);
-        var (,,,quantity2) = cnx.prizes(1);
-        assertEq(uint(quantity2), 1);
-
+    function testAcceptExchange() {
         assertEq(cnx.balanceOf(u1), 10000 * 10 ** 18);
-        u1.exchangePrize(1);
+        var (,,,exchanged) = cnx.requests(1);
+        assert(!exchanged);
+        u1.acceptExchange(1);
         assertEq(cnx.balanceOf(u1), 9000 * 10 ** 18);
-        var (,,,quantity3) = cnx.prizes(1);
-        assertEq(uint(quantity3), 0);
+        var (,,,exchanged2) = cnx.requests(1);
+        assert(exchanged2);
     }
 
-    function testFailExchangePrizeNoStarted() {
-        cnx.exchangePrize(1);
+    function testFailAcceptExchangeTwoTimes() {
+        u1.acceptExchange(1);
+        u1.acceptExchange(1);
     }
 
-    function testFailExchangePrizeNoAuthed() {
-        cnx.startExchange();
-        u3.exchangePrize(1);
+    function testFailAcceptExchangeOtherReceiver() {
+        u1.acceptExchange(1);
+        u2.acceptExchange(1);
+    }
+
+    function testFailAcceptExchangeNotEnoughBalance() {
+        cnx.addRequestExchange('Iphone 7', 'http://blahblah', 1000 * 10 ** 18, u2);
+        u2.acceptExchange(1);
+    }
+
+    function testFailAcceptExchangeNoAuthed() {
+        cnx.transfer(u3, 10000 * 10 ** 18);
+        cnx.addRequestExchange('Iphone 7', 'http://blahblah', 1000 * 10 ** 18, u3);
+        u3.acceptExchange(1);
     }
 }
 
 contract FakePerson {
     Cnx cnx;
+    DSVault vault;
 
-    function FakePerson(Cnx _cnx) {
+    function FakePerson(Cnx _cnx, DSVault _vault) {
         cnx = _cnx;
+        vault = _vault;
     }
 
     function transfer(address receipt, uint128 amount) {
         cnx.transfer(receipt, amount);
     }
 
-    function addPrize(string title, string photo, uint128 cost, uint8 quantity) {
-        cnx.addPrize(title, photo, cost, quantity);
+    function push(address receipt, uint128 amount) {
+        vault.push(receipt, amount);
     }
 
-    function editPrize(uint256 id, string title, string photo, uint128 cost, uint8 quantity) {
-        cnx.editPrize(id, title, photo, cost, quantity);
+    function addRequestExchange(string title, string photo, uint128 cost, address receiver) {
+        cnx.addRequestExchange(title, photo, cost, receiver);
     }
 
-    function exchangePrize(uint256 id) {
-        cnx.exchangePrize(id);
+    function editRequestExchange(uint256 id, string title, string photo, uint128 cost, address receiver) {
+        cnx.editRequestExchange(id, title, photo, cost, receiver);
+    }
+
+    function acceptExchange(uint256 id) {
+        cnx.acceptExchange(id);
     }
 }
